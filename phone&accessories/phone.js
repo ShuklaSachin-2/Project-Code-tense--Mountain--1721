910eac3f4e3b544d8ce4fc1f059548f1b39ad334
karmveer7 <mybestdhoni@gmail.com>
2022-07-23 15:50:13 +0530

    Day 3

---
+++ phone&accessories/phone.js
@@ -0,0 +1,479 @@
+var totalArr1=[
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/hsuak6etlkjhpxog1e1u.jpg",
+      
+        
+        title:"PIX NII:Most Powerful Mini PC with Dual 4K HDMI",
+        p:"0.35lb | Intel N6005 3.3GHz | 16GB+1TB|WI-FI6/BT5.2| 2x 4k@60Hz HDMI | Rj45 2.5G| 3x..",
+      
+        
+
+        price:"$328,851 HKD raised",
+        percent:"51%",
+
+        available: "",
+        // non:"",
+
+        p3:"28 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"PHONES & ACCESSORIES",
+
+
+    
+    },
+
+
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/vuhdhxrvgagorxi07vr7.jpg",
+        p1:"FUNDING",
+        p2:  "",
+        
+        title:"Help Us Complete THE QUEST:A New Musical",
+        p:"THE QUEST,a new musical from Murray Foster & Kieren MacMillan,needs your help!",
+        h1:"DANCE & THEATER",
+        
+
+        price:"$4,105 CAD raised",
+        percent:"41%",
+
+        available: "",
+        // non:"",
+
+        p3:"22 days left"
+    
+    },
+
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/j2ubhod76cmnjxojnptu.jpg",
+        box:"funding",
+
+        title:"Pericles for Ukraine",
+        p:"We are giving direct support & hope to Ukrainian refugee families with our performances of Pericles.",
+
+        price:"$1,975 GBP raised",
+        percent:"56%",
+
+        available: "",
+        // non:"",
+
+        p3:"51 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"DANCE & THEATER",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/et69lrwkihzrvk2oi3b1.jpg",
+        title:"Graceland Too Play at New York Theater Festival",
+        p:"The play Graceland Too inspired by the true story goes to the New York Theater Festival,May 2022!",
+
+        price:"$8,980 USD raised",
+
+        percent:"58%",
+
+        available: "",
+        non:"",
+
+        p3:"19 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"DANCE & THEATER",
+
+    
+    },
+
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/xrw8wamchxhlkefosbbe.jpg",
+        title:"Pro1 X Smartphone Functionality,Choice &...",
+        p:"A keyboard smartphone purposely designed for functionality,running Android,LIneage or Ubuntu",
+
+        price:"$1,469302 USD raised",
+        percent:"993%",
+
+        available: "",
+        non:"",
+
+        p3:"Now funding through InDemand",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/asl2sdmitlkm8j0su23z.jpg",
+        title:"Fash Pro: World's Fastest Powerbank Redesigned",
+        p:"25000mAh/92.5Wh|OLED Screen|3 USB-C Ports|Magsafe+Apple Watch|6 Ports|190 W..",
+
+        price:"$5,43,118 USD raised",
+        percent:"2220%",
+
+        available: "",
+        non:"",
+
+        p3:"Now funding through InDemand",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/ec2hgzd1fxjchuko4ufo.jpg",
+        title:"SuperMini GO: Versatile Stylish Wireless",
+        p:"Magnetic Wireless Charging | Type-C 20W PD | LCD Display | 10,000mAh | Perfect for Home",
+
+        price:"$111,010 USD raised",
+        percent:"2220%",
+
+        available: "",
+        non:"",
+
+        p3:"30 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/emgbz0sjb6jkbqmksd4p.jpg",
+        title:"Dockcier-Thunderbolt 3 Dock with 8TB Storage",
+        p:"Connect with everything | SSD Enclosure | Dual 8K Output | PD 100W Charging | 15W Wireless",
+
+        price:"$5,098 USD raised",
+        percent:"2007%",
+
+        available: "",
+        non:"",
+
+        p3:"Now funding through InDemand",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/esofnw0kd1bbmwy2t4lr.jpg",
+        title:"Omega: World's Smallest 100W & 200W USB-C",
+        p:"Revolutionary 100W & 200W credit card-size charger | Quick charge 4 devices at the same time",
+
+        price:"$5,098 USD raised",
+        percent:"12902%",
+
+        available: "",
+        non:"",
+
+        p3:"Now funding through InDemand",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/ikb6g69vxvw3lhzvmzva.jpg",
+        title:"GIGA: The Ultimate Universal GaN Charger",
+        p:"The Must HAVE Charger for ALL Your Powerful Devices | Super Fast Charge 4 Devices Simultaneously.",
+
+        price:"$5,098 USD raised",
+        percent:"2970%",
+
+        available: "",
+        non:"",
+
+        p3:"Now funding through InDemand",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/wr8mpwxeflfvsfdjbi2x.jpg",
+        title:"Q09: 3IN1 Gimbal Stabilizer Meets all You Need",
+        p:"Fill Light / Steady Shoot / Self-contained APP to assist shooting / Remote Control / Portable",
+
+        price:"$27700 USD raised",
+        percent:"139%",
+
+        available: "",
+        non:"",
+
+        p3:"14 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/fxji3ughyfbvejyp0qnh.jpg",
+        title:"inCharge XL, Making All Other Cables Obsolete.",
+        p:"A single universal cable for every device | 100W | OTG charging | 30cm to 3m",
+
+        price:"$5,098 USD raised",
+        percent:"7383%",
+
+        available: "",
+        non:"",
+
+        p3:"Now funding through on demand",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/dean7svtxdvyqfro0sxj.jpg",
+        title:"MEALZTIME - Cordless Self-heating Bento Box",
+        p:"One touch heating | Cordless self-heating | No water required | Reheats up to 5 meals per charge",
+
+        price:"$1,956,301 HKD raised",
+        percent:"6188%",
+
+        available: "",
+        non:"",
+
+        p3:"19 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/t2in3pxv2xlrf7a5m2iq.jpg",
+        title:"SHARGEEK Retro 35 GaN Charger",
+        p:"A homage to the classics |MAX 35W output| Charge notebook at high speed",
+
+        price:"$837,575",
+        percent:"44,334%",
+
+        available: "",
+        non:"",
+
+        p3:"Now funding through InDemand",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phones & Accessories",
+
+    
+    },
+
+  
+
+     
+
+  
+     
+
+    
+
+    
+   
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/aoaouifai01j3z4yqqej.jpg",
+        title:"The Momversations Project",
+        p:"Make your Mama product!Fund this project about moms created BY moms!",
+
+        price:"$4,003 USD raised",
+        percent:"73%",
+
+        available: "",
+        non:"",
+
+        p3:"1 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"Phone & Accessories",
+
+    
+    },
+   
+   
+   
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/c4kclp9lj7dgufhykfqa.jpg",
+        title:"An Evening with Paul Lynde Tour",
+        p:"Channeling the funny spirit of legendary comedian aul Lynde-Michael Airington brings him back!",
+
+        price:"$1,925",
+        percent:"39%",
+
+        available: "",
+        non:"",
+
+        p3:"Ending",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"DANCE & THEATER",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/aljikhekih5pstvf37op.jpg",
+        title:"BRED:Perfrmance,and profits,for people.",
+        p:"Fund an experiment in performence that pays the audience:art as income..",
+
+        price:"$4 GBP raised",
+        percent:"",
+
+        available: "",
+        non:"",
+
+        p3:"35 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"DANCE & THEATER",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/dppgyj8wspjzkbfnhlq8.jpg",
+        title:"Isc Presents:Love's Labour's Lost",
+        p:"join us this summer for our 2nd annual Summer Shakespeare Festival!",
+
+        price:"$1,627 USD raised",
+        percent:"",
+
+        available: "",
+        non:"",
+
+        p3:"24 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"DANCE & THEATER",
+
+    
+    },
+    {
+        image:"https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_273,w_273/c76nhy8ls1ueoiioyubn.jpg",
+        title:"Euphoria Theatre:The Destination of This Train Is",
+        p:"join us leading actionable change by helping brings this new play to the stage!",
+
+        price:"$1,100 USD raised",
+        percent:"",
+
+        available: "",
+        non:"",
+
+        p3:"33 days left",
+        p1:"FUNDING",
+        p2:  "",
+        h1:"DANCE & THEATER",
+
+    
+    },
+    
+     
+
+
+]
+function append(totalArr1){
+    totalArr1.forEach(function(elem){
+        let cont=document.createElement("div")
+        cont.setAttribute("id","nimg")
+
+        let image=document.createElement("img");
+        image.src=elem.image;
+        image.setAttribute("id","my-img1");
+
+       
+        let box=document.createElement("div");
+         box.setAttribute("id","fund-1");
+
+        let box1=document.createElement("div");
+        box1.setAttribute("id","m-di1");
+        box1.innerText="FUNDING";
+
+
+        let box2=document.createElement("div");
+        
+        box2.setAttribute("id","s2-div1");
+        box2.innerText=elem.p2;
+        box.append(box1,box2);
+
+       
+       
+        let h=document.createElement("hr");
+        h.setAttribute("id","hr-2");
+
+
+
+         let title=document.createElement("h3");
+        title.innerText=elem.title;
+        title.setAttribute("id","my-t2");
+
+        let p1=document.createElement("p");
+        p1.innerText=elem.p;
+        p1.setAttribute("id","p1-s1");
+
+
+        let h1=document.createElement("h5");
+        h1.innerText=elem.h1;
+        h1.setAttribute("id","h1-p1")
+
+       
+      
+      
+        let mybox=document.createElement("div");
+       box.setAttribute("id","mybox-11");
+
+       let mybox1=document.createElement("div");
+       mybox1.setAttribute("id","mb-21");
+       mybox1.innerText=elem.price;
+
+
+       let mybox2=document.createElement("div");
+       
+       mybox2.setAttribute("id","mb-31");
+       mybox2.innerText=elem.percent;
+
+        mybox.append(mybox1,mybox2);
+
+
+
+
+
+        let aval=document.createElement("p");
+        aval.innerText=elem.available;
+        aval.setAttribute("id","aval-1");
+
+        // let non=document.createElement("p")
+        // non.innerText=elem.non;
+        // non.setAttribute("id","non");
+
+
+
+
+
+
+
+
+        let p3=document.createElement("p");
+        p3.innerText=elem.p3;
+        p3.setAttribute("id","p3-3")
+
+
+
+
+
+
+
+
+
+
+
+        cont.append(image,box,h,title,p1,h1,mybox,aval,p3)
+
+        document.querySelector("#phonemycontainer2").append(cont);
+
+    })
+
+}
+append(totalArr1);
